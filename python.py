17cabe9065a713f82180c586d275c885c4437479
Abhishek Das <legokidffx@gmail.com>
2017-08-25 15:22:52 -0400

    Started writing code

---
+++ python.py
@@ -0,0 +1,12 @@
+def initial():
+    print "Welcome to The Club"
+    accountyn = raw_input("Do you have an account? Y/N")
+    accountyn = accountyn.lower()
+    if accountyn = y:
+        login()
+    else:
+        signup()
+
+def signup():
+    print "Start by choosing a username."
+    newusername = raw_input
