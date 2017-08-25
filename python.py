4a14b35ab83aa395ad9f97b8e11141e97dab757a
Abhishek Das <legokidffx@gmail.com>
2017-08-25 16:26:47 -0400

    tested

--- python.py
+++ python.py
@@ -1,8 +1,10 @@
 from tinydb import TinyDB, Query
 import getpass
+userinfo = TinyDB("userinfo.json")
+
 def initial():
     print "Welcome to The Club"
-    accountyn = raw_input("Do you have an account? Y/N")
+    accountyn = raw_input("Do you have an account? Y/N ")
     accountyn = accountyn.lower()
     if accountyn == "y":
         login()
@@ -12,12 +14,10 @@ def initial():
         initial()
 
 def signup():
-    
     print "Start by choosing a username."
     newusername = raw_input("Username:")
     print "Now choose a password."
     newpassword = getpass.getpass("Password:")
-    userinfo = TinyDB("userinfo.json")
     userinfo.insert({newusername: newpassword})
-    print userinfo.all()
 
+initial()
